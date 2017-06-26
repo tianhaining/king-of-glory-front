2d5250cbc49623b1088f2f7f80a352ccec073ce0
tianhaining <tianhaining@miaozhen.com>
2017-06-26 15:12:23 +0800

    建立工程

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import MuseUI from 'muse-ui'
+Vue.config.productionTip = false
+
+Vue.use(MuseUI);
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
